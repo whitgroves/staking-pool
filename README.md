3cd32e5e7df7525f734e5d85528d099083e499ca
whitgroves <whitney.groves@gmail.com>
2025-06-02 19:03:39 -0500

    wdg commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Staking Pool
+
+A smart contract designed to stake any ERC20 token and distribute deposits of said token according to stake size.
+
+The idea is to pool a specific token where holders can stake their share and the contract owner (or their delegates) can deposit and distribute additional tokens while ensuring that stakes can only be withdrawn by the addresses that staked them.
+
+Stakers deposit their tokens via `StakingPool.stake()`, withdraw them with `StakingPool.unstake()`, and can view their balance with `StakingPool.stakeSize()`. Note that `stake()` requires a spending allowance on the token contract via `ERC20.approve()`.
+
+Tokens deposited via `ERC20.transfer()` and `ERC20.transferFrom()` are automatically distributed when the owner or one of their delegates calls `StakingPool.distribute()`, and can only be withdrawn by stakeholders in proportion to their stakes. This is true even if the owner deactivates the pool.
+
+Deactivation only prevents adding new stakes to the pool. Users can still unstake, delegates can still distribute, and the owner can only move funds via `StakingPool.destake()`, which automatically transfers all stakes back to their holder's addresses.
+
+Even if the pool is permanently retired, the contract will make final calls to `distribute()` and `destake()` before granting the owner a spending allowance for any funds remaining due to rounding errors.
+
+In effect, this means that **if you transfer tokens directly into the pool, those tokens are *gone***, unless you are the only stakeholder (or on good terms with the rest of them). This is by design to prevent a single party from draining the pool.
+
+Finally, note that once the ERC20 token is designated, it cannot be changed. If the token gets updated and redeployed to a new address, the owner will need to deploy and activate a new pool pointing at the updated token contract, notify users of the change, then deactivate and permanently close the current pool with `StakingPool.retire()`.
--- StakingPool.sol
+++ StakingPool.sol
@@ -10,62 +10,77 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
  *
  * The contract owner cannot make withdrawals outside of their own staked tokens, but can deactivate and then
  * destake the pool to auto-transfer funds back to stakeholders.
+ *
+ * The staking pool is inactive by default.
  */
 contract StakingPool {
     
     address public owner;
     bool public active;
+    bool public retired;
     address public tokenAddress;
     uint public totalStaked;
-    mapping(address depositor => bool active) public depositors;
+    mapping(address delegate => bool active) public delegates;
 
     address[] private _stakers;
     mapping(address staker => uint tokens) private _stake;
+    uint8 private _warnings;
 
     constructor(address _tokenAddress) {
         require(IERC20(_tokenAddress).totalSupply() > 0, "Token must have a supply to stake.");
         tokenAddress = _tokenAddress;
         owner = msg.sender;
-        depositors[owner] = true;
+        delegates[owner] = true;
     }
 
-    function deposit(uint amount) public payable {
-        require(active == true, "Staking pool is inactive. Contact owner to (re)activate.");
-        require(depositors[msg.sender] == true, "Deposit failed. Contact owner to be added as a depositor.");
-        require(IERC20(tokenAddress).transferFrom(msg.sender, address(this), amount), "Deposit failed. Review sender balance and approvals.");
-        for (uint i = 0; i < _stakers.length; i++) {
-            uint stake_ = _stake[_stakers[i]];
-            if (stake_ == 0) continue;
-            uint dividend = amount * stake_ / totalStaked;
-            _stake[_stakers[i]] += dividend;
-        }
-        totalStaked += amount;
-    }
-
-    function stake(uint amount) external payable {
-        require(active == true, "Staking pool is inactive. Contact owner to (re)activate.");
-        require(IERC20(tokenAddress).transferFrom(msg.sender, address(this), amount), "Staking failed. Review sender balance and approvals.");
+    function stake(uint amount) external returns (bool) {
+        require(active == true, "Staking pool is inactive. Pre-staked funds may still be withdrawn.");
+        require(IERC20(tokenAddress).transferFrom(msg.sender, address(this), amount), 
+                "Staking failed. Review sender balance and approvals.");
         if (_stake[msg.sender] == 0) _stakers.push(msg.sender);
         _stake[msg.sender] += amount;
         totalStaked += amount;
+        return true;
     }
     
-    function unstake(uint amount) external payable {
+    function unstake(uint amount) external returns (bool) {
         require(amount <= _stake[msg.sender], "Unstaking failed. Amount exceeds stake size.");
-        require(IERC20(tokenAddress).transfer(msg.sender, amount), "Unstaking failed. Review staking pool balance.");
+        require(IERC20(tokenAddress).transfer(msg.sender, amount), 
+                "Unstaking failed. Review staking pool balance and contact contract owner.");
         _stake[msg.sender] -= amount;
         totalStaked -= amount;
+        return true;
     }
 
-    function destake() external {
-        require(active == false, "Cannot destake active pool.");
+    function distribute() public returns (bool) {
+        require(delegates[msg.sender] == true, "Delegates only. Contact owner to be added as a delegate.");
+        uint totalDistribution = IERC20(tokenAddress).balanceOf(address(this)) - totalStaked;
+        uint totalDistributed = 0;
+        for (uint i = 0; i < _stakers.length; i++) {
+            uint stake_ = _stake[_stakers[i]];
+            if (stake_ == 0) continue;
+            uint scale = (stake_ * 1e18) / totalStaked;
+            uint distribution = (totalDistribution * scale) / 1e18;
+            _stake[_stakers[i]] += distribution;
+            totalDistributed += distribution;
+        }
+        totalStaked += totalDistributed;
+        return true;
+    }
+
+    function destake() public returns (bool) {
         require(msg.sender == owner, "Owner only.");
+        require(active == false, "Staking pool is still active. Deactivate first.");
         for (uint i = 0; i < _stakers.length; i++) {
             address staker = _stakers[i];
             uint stake_ = _stake[staker];
             if (stake_ == 0) continue;
-            if (IERC20(tokenAddress).transfer(staker, stake_)) _stake[staker] = 0;
+            if (IERC20(tokenAddress).transfer(staker, stake_)) {
+                _stake[staker] = 0;
+                totalStaked -= stake_;
+            }
         }
+        return true;
     }
 
     function stakeSize() external view returns (uint) {
@@ -73,38 +88,56 @@ contract StakingPool {
     }
 
     function stakeSizeFor(address staker) external view returns (uint) {
-        require(msg.sender == owner, "Owner only.");
+        require(delegates[msg.sender] == true, "Delegates only.");
         return _stake[staker];
     }
 
-    function activate() external {
+    function activate() external returns (bool) {
+        require(msg.sender == owner, "Owner only.");
         require(active == false, "Staking pool is already active.");
-        require(msg.sender == owner, "Owner only.");
+        require(retired == false, "Staking pool has been permanently retired.");
         active = true;
+        return true;
     }
 
-    function deactivate() external {
+    function deactivate() external returns (bool) {
+        require(msg.sender == owner, "Owner only.");
         require(active == true, "Staking pool is already inactive.");
-        require(msg.sender == owner, "Owner only.");
         active = false;
+        return true;
     }
 
-    function transferOwner(address newOwner) external {
-        require(active == true, "Staking pool is inactive. (Re)activate first.");
+    function retire() external returns (bool) {
         require(msg.sender == owner, "Owner only.");
+        require(active == false, "Cannot retire an active pool.");
+        require(distribute(), "Final distribution failed.");
+        require(destake(), "Final destaking failed.");
+        retired = true;
+        IERC20 token = IERC20(tokenAddress);
+        uint remainder = token.balanceOf(address(this)) - totalStaked;
+        token.approve(msg.sender, remainder);
+        return true;
+    }
+
+    function transferOwner(address newOwner) external returns (bool) {
+        require(msg.sender == owner, "Owner only.");
+        require(active == true, "Staking pool is inactive. (Re)activate first.");
         owner = newOwner;
+        return true;
     }
 
-    function addDepositor(address depositor) external {
-        require(active == true, "Staking pool is inactive. (Re)activate first.");
+    function addDelegate(address delegate) external returns (bool) {
         require(msg.sender == owner, "Owner only.");
-        depositors[depositor] = true;
+        require(active == true, "Staking pool is inactive. (Re)activate first.");
+        delegates[delegate] = true;
+        return true;
     }
 
-    function removeDepositor(address depositor) external {
-        require(active == true, "Staking pool is inactive. (Re)activate first.");
+    function removeDelegate(address delegate) external returns (bool) {
         require(msg.sender == owner, "Owner only.");
-        depositors[depositor] = false;
+        require(active == true, "Staking pool is inactive. (Re)activate first.");
+        delegates[delegate] = false;
+        return true;
     }
 
 }
